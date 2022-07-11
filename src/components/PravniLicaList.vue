44b55a7253805ea4d353c7097a2df8aa77f7dffd
Jocka13 <joce.velkovski33@gmail.com>
2022-07-11 17:37:15 +0200

    finished

--- src/components/PravniLicaList.vue
+++ src/components/PravniLicaList.vue
@@ -35,10 +35,10 @@ data(){
           text: 'Аналитички заклучен лист за тековната година со последен датум на книжење'
         },
         {
-          text: 'Согласност за извадок од Македонско Кредитно Биро'
+          text: 'Согласност за извадок од Македонско Кредитно Биро <a target="_blank" href="https://s-leasing.mk/content/%D0%A1%D0%BE%D0%B3%D0%BB%D0%B0%D1%81%D0%BD%D0%BE%D1%81%D1%82%20%D0%B7%D0%B0%20%D0%B8%D0%B7%D0%B2%D0%B0%D0%B4%D0%BE%D0%BA%20%D0%BE%D0%B4%20%D0%9C%D0%B0%D0%BA%D0%B5%D0%B4%D0%BE%D0%BD%D1%81%D0%BA%D0%BE%20%D0%9A%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D0%BD%D0%BE%20%D0%91%D0%B8%D1%80%D0%BE.pdf">(<b>превземи согласност</b>)</a>'
         },
         {
-          text: 'Изјава за вистински сопственик'
+          text: 'Изјава за вистински сопственик <a target="_blank" href="https://s-leasing.mk/content/Izjava-za-vistinski-sopstvenik.pdf">(<b>превземи согласност</b>)</a>'
         }
       ]
     }
